157d5ad7fab7e0074799b80b8d36a2d60a437390
Marius Andra <marius.andra@gmail.com>
2020-12-30 20:46:52 +0100

    kib

--- frontend/src/scenes/tree/utils.ts
+++ frontend/src/scenes/tree/utils.ts
@@ -8,16 +8,14 @@
  *
  * @return Formatted string.
  */
-export function humanFileSize(bytes: number, si = false, dp = 1): string {
-    const thresh = si ? 1000 : 1024
+export function humanFileSize(bytes: number, si = false, dp = 2): string {
+    const thresh = 1024
 
     if (Math.abs(bytes) < thresh) {
         return bytes + ' B'
     }
 
-    const units = si
-        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    const units = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
     let u = -1
     const r = 10 ** dp
 
